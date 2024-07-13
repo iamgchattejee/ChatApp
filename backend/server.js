36fec1cf1997ee8d27c8b70708e8d5fb213590f5
Gourab Chatterjee <iamgchattejee@gmail.com>
2024-07-13 21:54:07 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { app, server } from "./socket/socket.js";
 import cors from "cors";
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5000",
     methods: ["GET", "POST"],
     credentials: true
 };
