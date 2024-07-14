dd265c908ce6cb6fcc27813f67a7dcda40b8f5fd
Gourab Chatterjee <iamgchattejee@gmail.com>
2024-07-14 11:48:47 +0530

    Deployment

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -3,8 +3,7 @@ import MessageContainer from '../../components/MessageContainer';
 
 export const Homepage = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
-    backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className="flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <Sidebar/>
         <MessageContainer/>
     </div>
